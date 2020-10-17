e5fe6d902aa5cfe94b781161d3466b039af9d349
FotisAndritsos <fotisandritsos85@gmail.com>
2020-10-17 22:54:00 +0300

    half way

--- main.js
+++ main.js
@@ -3,22 +3,28 @@ const btnDel = document.getElementById('bntDel');
 const addBtn = document.getElementById('addBtn');
 const todo = document.getElementById('todo');
 const con = document.querySelector('.container');
+const msg = document.querySelector('.mesge');
 
 const toDoit = (event) =>{
     event.preventDefault();
+    if(todo.value){
+        const aTodos = document.createElement("div");
+        aTodos.classList.add("todos");
+        con.appendChild(aTodos);
+        
     
-    const aTodos = document.createElement("div");
-    aTodos.classList.add("todos");
-    con.appendChild(aTodos);
+        // todolist
+        const aparTodo = document.createElement("p");
+        aparTodo.classList.add("par-todo");
+        aparTodo.innerText = todo.value;
+        aTodos.appendChild(aparTodo);
     
-
-    // todolist
-    const aparTodo = document.createElement("p");
-    aparTodo.classList.add("par-todo");
-    aparTodo.innerText = todo.value;
-    aTodos.appendChild(aparTodo);
-
-    todo.value = "";
+        todo.value = "";
+        msg.innerText = "";
+    }else{
+       msg.innerText = "Please enter a To do first..."
+    }
+    
 };
 
 
